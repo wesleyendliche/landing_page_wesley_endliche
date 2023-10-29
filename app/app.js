3da1f618f55b2df7bbe967239841e0478cbdc7eb
Wesley Endliche <wesleyhoo@hotmail.com>
2023-10-29 17:46:22 -0300

    :art:Estiliza a seção cursos

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var myStoryBtn = document.getElementById(expand);
+var myStoryDiv = document.getElementById(myStory);
+
+function expandText() {    
+    if (myStoryDiv.style.display = "none") {
+        myStoryDiv.style.display = "block";
+    } else {
+        myStoryDiv.style.display = "none";
+    }
+}
+
